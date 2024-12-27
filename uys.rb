09f58afed944f7adc0a30f2555f79c66e2d8f55f
Dan Higgins <13562502+lilole@users.noreply.github.com>
2024-12-26 19:56:04 -0600

    Multiple updates
    - Always use a PTY to run bash cmds with default to page.
    - Add `String#ellipt` to chop to width with ellipsis in center.
    - Generic nestable `#with_temp_file`.
    - Factoring and add uninstalled/installed indicator on pkg cache clean.

--- uys.rb
+++ uys.rb
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 
 require "io/console"
+require "io/wait"
+require "pty"
 require "set"
 require "shellwords"
 
@@ -54,8 +56,28 @@ module Uys
 
   module Extensions
     def self.apply
-      ::Object.instance_methods.member?(:as_struct) or ::Object.include(Object::AsStruct)
+      ::Object.include(Object::AsStruct)
+      ::String.include(String::Ellipt)
     end
+
+    module String
+      module Ellipt
+        def ellipt!(width, ellipsis="...")
+          e_sz = ellipsis.size
+          return replace(ellipsis) if width <= e_sz
+          return self if size <= width
+          chunk, carry = (width - e_sz).divmod(2)
+          (0...e_sz).each  { |i| self[chunk + carry + i] = ellipsis[i] }
+          (0...chunk).each { |i| self[chunk + carry + e_sz + i] = self[size - chunk + i] }
+          slice!(width, size - width)
+          self
+        end
+
+        def ellipt(width, ellipsis="...")
+          dup.ellipt!(width, ellipsis)
+        end
+      end # Ellipt
+    end # String
 
     module Object
       module AsStruct
@@ -74,7 +96,7 @@ module Uys
 
   # ___________________________________________________________________________
 
-  Extensions.apply
+  Extensions.apply # Needed for `Config` def
 
   Config = {
     boot_log: false,
@@ -101,26 +123,61 @@ module Uys
   # ___________________________________________________________________________
 
   module Util
-    def cmd(*strings, echo: true, page: nil, say_done: true)
-      script = disp = strings.join
-      disp = disp.inspect if disp.include?("\n")
-      less = page ? +" | less -FIJMRSWX#8 --status-col-width=1" : nil
-      if less
-        less << " #{page}" if String === page
-        disp = "{ #{disp}; } 2>&1#{less}"
-      end
+    def cmd(*strings, echo: true, page: true, say_done: true)
+      script = strings.join.gsub(/\A\s+|\n+\z/, "") << "\n"
+      script << "echo '+ Done.'\n" if say_done
+      less_opts = page ? %w[-FIJMrSWX#8 --status-col-width=1] : nil
+      less_opts.concat(page.strip.split) if String === page
+
       if echo
-        say_done and script = "#{script}; echo '+ Done.'"
-        script = "{ echo '++ '#{disp.shellescape}; #{script}; } 2>&1#{less}"
-      else
-        say_done and script = "{ #{script}; echo '+ Done.'; }"
-        script = "#{script} 2>&1#{less}"
+        less = page ? " | less #{less_opts.shelljoin}" : ""
+        disp = "++ {\n#{script}} 2>&1#{less}".inspect[1..-2]
+        maxw = (Integer === echo) ? echo : (page ? 199 : 80)
+        disp.ellipt!(maxw)
+        script = "echo #{disp.shellescape}\n#{script}"
       end
-      system(["/usr/bin/bash", "#{self.class.name}#cmd bash"], "-c", script)
+
+      with_temp_file("{\n#{script}}") do |file|
+        args = [["/usr/bin/bash", "#{self.class.name}#cmd bash"], file]
+        PTY.spawn(*args) do |pt_out, pt_in, script_pid|
+          if page
+            final_o = IO.popen(["less"].concat(less_opts), "w")
+          else
+            final_o = $stdout
+          end
+
+          outputter = Thread.new do
+            loop do
+              begin
+                final_o.write(pt_out.read(1))
+              rescue Errno::EIO
+                break
+              end
+            end
+          end
+
+          inputter = Thread.new do
+            if ! page
+              $stdin.raw do
+                loop do
+                  break if ! outputter.alive?
+                  pt_in.write($stdin.getch) if $stdin.wait_readable(0.1)
+                end
+              end
+            end
+          end
+
+          Process.wait(script_pid)
+          $?.tap do |_result|
+            [outputter, inputter].each(&:join)
+            final_o.close if page
+          end
+        end
+      end
     end
 
     def ask_continue(prompt="Continue?", opts="Ynq")
-      def_reply = opts.gsub(/[a-z]+/, "")
+      def_reply = opts.gsub(/[^A-Z]+/, "")
       raise "Only 1 uppercase is allowed: #{opts.inspect}" if def_reply.size > 1
       $stderr.puts("")
       until nil
@@ -134,6 +191,22 @@ module Uys
       $stderr.puts("")
       exit if lreply == "q"
       lreply == "y"
+    end
+
+    def with_temp_file(body="")
+      @with_temp_file_count ||= 0
+
+      begin
+        c = (@with_temp_file_count += 1)
+        file = "/tmp/#{File.basename($0)}.#{__id__}.#{c}.tmp"
+      end while File.exist?(file)
+
+      File.write(file, body)
+      begin
+        yield(file)
+      ensure
+        File.delete(file) rescue nil
+      end
     end
   end # Util
 
@@ -180,30 +253,30 @@ module Uys
     end
 
     def check_all_updates
-      cmd("{ checkupdates; echo; #{pikaur("-Qu")}; }", page: true)
+      cmd("checkupdates \necho \n#{pikaur("-Qu")}")
     end
 
     def check_pacman_updates
-      cmd("checkupdates", page: true)
+      cmd("checkupdates")
     end
 
     def pacman_pre_update
       if (pkgs = config.pacman.pre_update.uninstalls)&.any?
         if ask_continue("Uninstall #{pkgs.inspect}?", "yNq")
-          cmd(pacman("-Rs #{pkgs.shelljoin}"), page: "-+F -r")
+          cmd(pacman("-Rs #{pkgs.shelljoin}"))
         end
       end
     end
 
     def download_pacman_updates
       ask_continue("Download needed updates?") or return
-      cmd(pacman("-Suyw"), page: "-+F -r +F")
+      cmd(pacman("-Suyw"))
     end
 
     def apply_pacman_updates
       puts("\nWARNING: CLOSE EXTRA APP WINDOWS: Updates are about to be applied.")
       ask_continue("Apply downloaded updates?") or return
-      cmd(pacman("-Su"), page: "-+F -r")
+      cmd(pacman("-Su"))
     end
 
     def check_for_pacnew_files
@@ -230,31 +303,31 @@ module Uys
       res = [/warn|error|fail|fatal/i, / kernel: Linux version .+ SMP /]
       out = `journalctl --boot --no-hostname --priority=0..5` # emerg..notice
       out = out.each_line.select { |line| res.any? { _1.match?(line) } }.join
-      cmd("echo #{out.shellescape}", page: "+G", echo: false)
+      with_temp_file(out) { |file| cmd("cat #{file}", page: "+G", echo: true) }
     end
 
     def apply_new_pacman_updates
       ask_continue("Check and apply new pacman updates?") or return
-      cmd(pacman("-Suy"), page: "-+F -r")
+      cmd(pacman("-Suy"))
     end
 
     def check_pikaur_updates
       ask_continue("Check pikaur updates?") or return
       loop do
-        cmd(pikaur("-Qu"), page: "-r")
+        cmd(pikaur("-Qu"))
         ask_continue("Check again?", "yNq") or break
       end
     end
 
     def apply_pikaur_updates
       ask_continue("Apply pikaur updates?", "yNq") or return
-      cmd(pikaur("-Su"), page: "-+F -r")
+      cmd(pikaur("-Su"))
     end
 
     def pikaur_post_update
       if (pkgs = config.pikaur.post_update.installs)&.any?
         if ask_continue("Install #{pkgs.inspect}?", "yNq")
-          cmd(pikaur("-S #{pkgs.shelljoin}"), page: "-+F -r")
+          cmd(pikaur("-S #{pkgs.shelljoin}"))
         end
       end
     end
@@ -271,9 +344,9 @@ module Uys
 
     def rebooted? = !! config.rebooted
 
-    def pacman(opts) = "sudo pacman --noconfirm --color always #{opts}"
+    def pacman(opts) = "sudo pacman --noconfirm --color=always #{opts}"
 
-    def pikaur(opts) = "pikaur -a --noconfirm --color always #{opts}"
+    def pikaur(opts) = "pikaur -a --noconfirm --color=always #{opts}"
   end # Core
 
   # ___________________________________________________________________________
@@ -281,13 +354,12 @@ module Uys
   class PackageCacheClean
     include Util
 
-    attr_reader :cache_dirs, :dir_info, :file_info, :keep_installed, :keep_uninstalled, :pkg_info
+    attr_reader :cache_dirs, :file_info, :keep_installed, :keep_uninstalled
 
     def initialize(cache_dirs, keep_installed:, keep_uninstalled:)
       @cache_dirs       = cache_dirs
       @keep_installed   = keep_installed
       @keep_uninstalled = keep_uninstalled
-      @markedi = @markedu = 0
     end
 
     def run
@@ -300,8 +372,6 @@ module Uys
     end
 
     def find_package_files
-      @dir_info = Hash.new { |h, k| h[k] = [] }
-      @file_info = {}
       cache_dirs.each do |dir|
         dir = File.expand_path(dir)
         begin
@@ -315,32 +385,25 @@ module Uys
       end
     end
 
+    def dir_info = @dir_info ||= Hash.new { |h, k| h[k] = [] }
+
+    def file_info = @file_info ||= {}
+
     def sort_list_by_version
       index = -1
       dir_info.each do |dir, infos|
-        paths = infos.map { |h| h[:path] }
-
-        text = paths.join("\n") << "\n" # pacsort requires last LF
-        out = with_temp_file(text) { |file| `cat #{file} | pacsort --files` }
-
-        out.split("\n").each do |path|
+        paths = infos.map { |h| h[:path] }.join("\n") << "\n" # pacsort requires last LF
+        out = with_temp_file(paths) { |file| `pacsort --files < #{file}` }
+        out.strip.split("\n").each do |path|
           file_info[path][:index] = (index += 1)
         end
       end
-      @dir_info = nil # Release mem
-    end
-
-    def with_temp_file(body="")
-      file = "/tmp/PackageCacheClean.tmp"
-      File.write(file, body)
-      yield(file).tap { File.delete(file) rescue nil }
     end
 
     def group_list_by_pkg_name
-      @pkg_info = Hash.new { |h, k| h[k] = [] }
-      file_info.each do |path, info|
+      file_info.each do |path, finfo|
         begin
-          name = info[:file]
+          name = finfo[:file]
           raise if ! (rem = /\A(.+)-[^-]+\z/.match(name))
           parts = rem[1].split("-")
           raise if parts.size < 3
@@ -352,43 +415,67 @@ module Uys
         end
         file_info[path][:pkg]     = pname
         file_info[path][:version] = pver
-        pkg_info[pname] << file_info[path]
+        pkg_info[pname][:file_infos] << file_info[path]
+        pkg_info[pname][:installed]   = installed_pkgs.member?(pname)
       end
     end
 
-    def mark_list_by_keepu_rules
-      pkg_info.each_key do |pkg|
-        @markedu += mark_for_delete(pkg, keep: keep_uninstalled) if ! installed_pkgs.member?(pkg)
-      end
-    end
-
-    def mark_list_by_keepi_rules
-      pkg_info.each_key do |pkg|
-        @markedi += mark_for_delete(pkg, keep: keep_installed) if installed_pkgs.member?(pkg)
+    def pkg_info
+      @pkg_info ||= begin
+        Hash.new do |pinfo, pname|
+          pinfo[pname] = Hash.new do |h, k|
+            if   k == :file_infos then h[k] = []
+            else nil
+            end
+          end
+        end
       end
     end
 
     def installed_pkgs = @installed_pkgs ||= `pacman -Qsq`.split("\n").to_set
 
-    def remove_marked_files
-      marked = begin
-        file_info.select { |_path, info| info[:delete] }
-          .map { |path, _info| Dir.glob("#{path}*") } # Need glob for .sig files
-          .flatten
+    def mark_list_by_keepu_rules
+      @markedu = 0
+      pkg_info.each do |pkg, info|
+        @markedu += mark_for_delete(pkg, keep: keep_uninstalled) if ! info[:installed]
       end
+    end
 
-      text = []; del_bytes = 0
+    def mark_list_by_keepi_rules
+      @markedi = 0
+      pkg_info.each do |pkg, info|
+        @markedi += mark_for_delete(pkg, keep: keep_installed) if info[:installed]
+      end
+    end
+
+    def mark_for_delete(pkg, keep:)
+      return 0 if pkg_info[pkg][:file_infos].size < keep
+      marked = 0
+      sorted_infos = pkg_info[pkg][:file_infos].sort { |a, b| b[:index] <=> a[:index] }
+      sorted_infos[keep..-1].each { |info| info[:delete] = true; marked += 1 }
+      marked
+    end
+
+    def remove_marked_files
+      text = []; marked = []; del_bytes = 0
       text << "+ Total #{pkg_info.size} packages from #{file_info.size} files."
       text << "+ Marked #{@markedi} installed package files for delete."
       text << "+ Marked #{@markedu} uninstalled package files for delete."
-      marked.each do |path|
-        text << "+ Marked: #{path.inspect}" if path[-4, 4] != ".sig"
-        del_bytes += File.size(path)
+      file_info.select do |_path, finfo|
+        finfo[:delete]
+      end.each do |path, finfo|
+        Dir.glob("#{path}*").each do |path2| # Need glob for .sig files
+          marked << path2
+          del_bytes += File.size(path2)
+          next if path2[-4, 4] == ".sig"
+          type = pkg_info[finfo[:pkg]][:installed] ? "I" : "U"
+          text << "+ Marked: #{type}: #{path.inspect}"
+        end
       end
       text << "+ Total %3.1f MB (%d bytes) marked for delete." % [del_bytes / 1e6, del_bytes]
 
       with_temp_file(text.join("\n")) do |file|
-        cmd("cat #{file}", page: true, echo: false, say_done: false)
+        cmd("cat #{file}", echo: false, say_done: false)
       end
       return if marked.size == 0
       ask_continue "Are you sure?" or return
@@ -400,14 +487,6 @@ module Uys
       marked.any? and marked.each_slice(20) { |paths| `rm #{paths.shelljoin}` }
 
       puts "+ Done."
-    end
-
-    def mark_for_delete(pkg, keep:)
-      return 0 if pkg_info[pkg].size < keep
-      marked = 0
-      sorted_infos = pkg_info[pkg].sort { |a, b| b[:index] <=> a[:index] }
-      sorted_infos[keep..-1].each { |info| info[:delete] = true; marked += 1 }
-      marked
     end
   end # PackageCacheClean
 
