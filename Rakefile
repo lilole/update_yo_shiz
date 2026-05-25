a80f7dffa1d3ee7379c14e0acc6a5e7be1fceb72
Dan Higgins <13562502+lilole@users.noreply.github.com>
2026-05-25 14:08:30 -0500

    Reference Ultisel for extensions

--- Rakefile
+++ Rakefile
@@ -8,6 +8,7 @@ load "../ultisel/load/arma.rake" # ...sorry this is not public yet
 arma.import arma: "../ultisel", version: nil, build: true,
   include: [
     Ulse.minimum_custom_includes,
-    '^ulse/ext/string\.rb',
+    '^ulse/ext/(object|string)\.rb',
+    '^ulse/ext/object/(as_grouping|grouping|transform)\.rb',
     '^ulse/ext/string/ellipt\.rb'
   ]
--- lib/extensions.rb
+++ lib/extensions.rb
@@ -5,26 +5,7 @@
 
 module Extensions
   def self.apply
-    ::Object.include(Object::AsStruct)
-    ::String.include(::Ulse::Ext::String::Ellipt)
+    ::Ulse::Ext::Object::AsGrouping.apply
+    ::Ulse::Ext::String::Ellipt.apply
   end
-
-  module Object
-    module AsStruct
-      ### Recursively convert Hash-like objects within self to Struct objects,
-        # so that keys become accessible as method names.
-        # This is helpful because (a) code is more readable, and (b) access to
-        # Struct values is much faster than OpenStruct.
-        #
-      def as_struct
-        if respond_to?(:keys) && respond_to?(:values)
-          Struct.new(*keys.map(&:to_sym)).new(*values.map(&:as_struct))
-        elsif respond_to?(:map)
-          map(&:as_struct)
-        else
-          self
-        end
-      end
-    end # AsStruct
-  end # Object
 end
